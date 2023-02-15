db641e9eecca2ccfb29b79f6372ef39767802aa0
Dmitriy Nizhnik <94756210+DmitriyNizhn@users.noreply.github.com>
2023-02-15 23:15:34 +0300

    Lesson9 (#8)
    * lesson_9 Preporations for deployment
    
    * lesson_9 Load testing

--- config/celery.py
+++ config/celery.py
@@ -2,8 +2,11 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from django.conf import settings
 
-celery_app = Celery("braniac")  # возможно нужно прописать свое название "django"
+if settings.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+celery_app = Celery("braniac")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
